bc5ae130d203df238e74d4d90ddb1b0012cf4f66
Chris Farris <15526837+jchrisfarris@users.noreply.github.com>
2022-07-16 10:15:02 -0400

    You need to create detectors in the payer account too.

--- scripts/enable_guardduty_delegation.sh
+++ scripts/enable_guardduty_delegation.sh
@@ -13,4 +13,15 @@ REGIONS=`aws ec2 describe-regions --query 'Regions[].[RegionName]' --output text
 for r in $REGIONS ; do
   echo "Enabling GuardDuty Delegated Admin in $r"
   aws guardduty enable-organization-admin-account --admin-account-id $GD_ACCOUNT --region $r
+
+  DETECTOR=`aws guardduty list-detectors --query DetectorIds[] --output text --region $r `
+  if [ -z $DETECTOR ] ; then
+    echo "Creating a Detector in the Organizations Management account in $r"
+    DETECTOR=`aws guardduty create-detector  --output text --region $r --finding-publishing-frequency ONE_HOUR --enable`
+    if [ -z $DETECTOR ] ; then
+      echo "Failed to create a detector in $r. Aborting script"
+      exit 1
+    fi
+  fi
+
 done
